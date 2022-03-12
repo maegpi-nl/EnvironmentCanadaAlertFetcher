bf156a950c1c70f4e8e85bd5a62b05537e0bfc70
maegpi <memshey@mun.ca>
2022-03-12 17:22:39 -0330

    Removed prototyping code. Should be ready for implimentation testing

--- weather.py
+++ weather.py
@@ -1,50 +1,54 @@
+#
+
 import feedparser
 import requests
 from bs4 import BeautifulSoup
+import re
 
-class weatherAPI:
-    def __init__(self, entry):
+TEST_FLAG = True #Manual flag for local testing of file
+
+class scrapeAlerts:
+    def __init__(self):
+        self.w = weatherRSS()
+
+    def _isAlert(self):
+        return True if (self.w.getNumberEntries() > 1) else False
+
+    def getAlert(self):
+        if self._isAlert():
+            url = self.w.getLink(1)
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, "html.parser")
+            result = soup.find("div", {"class": "col-xs-12"})
+            alert = re.sub('<[^>]+>',"",result.text)
+            alertLink = self.w.getLink(1)
+            alertReport = alert.replace(".",". ") + alertLink
+            return alertReport
+        else:
+            return "No Current Alerts!\n"
+
+class weatherRSS:
+    def __init__(self):
         self.Feed = feedparser.parse("https://weather.gc.ca/rss/battleboard/nl21_e.xml")
-        self.entry = entry
-        
-    def getNumberEntries():
+
+    def getNumberEntries(self):
         return len(self.Feed.entries)
 
-    def getTitle():
-        return self.entry.title
+    def getTitle(self,i):
+        return self.Feed.entries[i].title
 
-    def getDate(entry):
-        return entry.summary
+    def getDate(self,i):
+        return self.Feed.entries[i].summary
 
-    def getLink(entry):
-        return entry.link
+    def getLink(self,i):
+        return self.Feed.entries[i].link        
 
-NewsFeed = feedparser.parse("https://weather.gc.ca/rss/battleboard/nl21_e.xml")
-print("Number of RSS entries:" ,len(NewsFeed.entries))
-
-entry = NewsFeed.entries[1]
-for key in entry.keys():
-    print(key)#,": ", NewsFeed.entries.key)
-print()
-
-for warning in NewsFeed.entries:
-    print(warning.summary)
-    print(warning.title)
-    print(warning.link)
-    print(warning.id)
-    print()
-#summary is the date and time the warning was posted
-
-numberOfEntries = len(NewsFeed.entries)
-
-if numberOfEntries > 1:
-    url = NewsFeed.entries[1].link
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    result = soup.find("div", {"class" : "col-xs-12"})
-    weatherWarning = result.find("p")
-    print(result)
-#    print(result.prettify())
-   # print(page.text)
-
+if __name__ == "__main__":
+    if TEST_FLAG:
+        plsWork = scrapeAlerts()
+        print(plsWork.getAlert())
+        with open("testFile.txt",'w') as fp:
+            fp.write(plsWork.getAlert())
+            print("Write Successful!\n")
+     
 
